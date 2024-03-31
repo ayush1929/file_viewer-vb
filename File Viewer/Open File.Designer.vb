30c4a4f5c5d1912b5efd85810a81358da794583a
Ayush Patel <patelayush1929@gmail.com>
2024-03-31 16:42:26 -0400

    Add project files.

---
+++ File Viewer/Open File.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFileContent = New System.Windows.Forms.TextBox()
+        Me.btnOpenFile = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtFileContent
+        '
+        Me.txtFileContent.Location = New System.Drawing.Point(107, 12)
+        Me.txtFileContent.Multiline = True
+        Me.txtFileContent.Name = "txtFileContent"
+        Me.txtFileContent.Size = New System.Drawing.Size(681, 411)
+        Me.txtFileContent.TabIndex = 0
+        '
+        'btnOpenFile
+        '
+        Me.btnOpenFile.Location = New System.Drawing.Point(12, 12)
+        Me.btnOpenFile.Name = "btnOpenFile"
+        Me.btnOpenFile.Size = New System.Drawing.Size(75, 23)
+        Me.btnOpenFile.TabIndex = 1
+        Me.btnOpenFile.Text = "Open File"
+        Me.btnOpenFile.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnOpenFile)
+        Me.Controls.Add(Me.txtFileContent)
+        Me.Name = "Form1"
+        Me.Text = "Open File"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtFileContent As TextBox
+    Friend WithEvents btnOpenFile As Button
+End Class
---
+++ File Viewer/Open File.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub btnOpenFile_Click(sender As Object, e As EventArgs) Handles btnOpenFile.Click
+        Dim openFileDialog As New OpenFileDialog()
+
+        ' Set the filter for text files
+        openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            Try
+                Dim filePath As String = openFileDialog.FileName
+
+                ' Read the content of the selected file and display it in the TextBox control
+                Dim fileContent As String = My.Computer.FileSystem.ReadAllText(filePath)
+                txtFileContent.Text = fileContent
+            Catch ex As Exception
+                MessageBox.Show("Error: " & ex.Message, "File Open Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+End Class
